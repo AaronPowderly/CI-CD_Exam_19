4e0fb0fff27c89ad033577cb43320a1083553e95
AARONPOWDERLY-STUDEN <G00434425@atu.ie>
2025-11-19 14:46:05 +0000

    added put to the the vehicle service and controller. also added pattern validation to reg

--- src/main/java/ie/atu/cicd1_exam_19/VehicleService.java
+++ src/main/java/ie/atu/cicd1_exam_19/VehicleService.java
@@ -1,4 +1,25 @@
 package ie.atu.cicd1_exam_19;
 
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class VehicleService {
+
+    public final List<Vehicle> store = new ArrayList<>();
+
+    public List<Vehicle> findAll() { return new ArrayList<>(store); }
+
+    public Optional<Vehicle> findByReg(String reg)
+    {
+        for(Vehicle v : store){
+            if(v.getReg().equals(reg)){
+                return Optional.of(v);
+            }
+        }
+        return Optional.empty();
+    }
 }
